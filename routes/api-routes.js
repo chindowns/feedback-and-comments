64575c66f1f35912a07eef0280acc25170e80b28
Michael Downs <michael@chindowns.com>
2020-08-14 07:02:47 -0700

    [TEST] insert with form-data vs body object

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,8 +3,16 @@ var db = require("../models");
 module.exports = (app) => {
     // Post Feedback
     app.post("/api/feedback", (req, res) => {
+        console.log("--------------req.body--------------------")
         console.log(req.body)
-        db.Feedback.create(req.body)
+        console.log("-------req----------")
+        console.log(req)
+        console.log("-----------------end-------------------------")
+        db.Feedback.create(req.body, {
+            site: req.body.site,
+            email: req.body.email,
+            feedback: req.body.feedback
+        })
             .then(result => { console.log(result); res.send("Feedback received") })
             .catch(err => { console.log(err); res.status(401).json(err) })
     });
